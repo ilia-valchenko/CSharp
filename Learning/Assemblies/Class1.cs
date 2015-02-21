e35ba30300014c3d40a1469e32542a4d081e2f58
ivalchenko <i.valchenk@hotmail.com>
2015-02-21 23:48:57 +0300

    Continue to learn C#

---
+++ Learning/Assemblies/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// new
+using System.Windows.Forms;
+
+namespace PersonLibrary
+{
+    public class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public Person() { }
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public void AboutPerson()
+        {
+            MessageBox.Show("Name: " + Name + "\nSurname: " + Surname + "\nAge: " + Age);
+        }
+    }
+}
