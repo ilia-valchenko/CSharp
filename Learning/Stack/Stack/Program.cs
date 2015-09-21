ea5f93549f5fa2df55ef33294fbd824798c75ab5
ivalchenko <i.valchenk@hotmail.com>
2015-09-21 21:19:13 +0300

    Added my stack realization on C#

---
+++ Learning/Stack/Stack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is my custom stack realization based on integer values.\nPlease enter the size of stack: ");
+
+            bool isStop = false;
+            int size = Convert.ToInt32(Console.ReadLine());
+            Stack stackTest = new Stack(size);
+
+            while (!isStop)
+            {
+                Console.WriteLine("\n\nThanks! Now you have to choose one of the proposed operations:\nPush: 1\nPop: 2\nPeek: 3\nStop: 4\n");
+                int operation = Convert.ToInt32(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case (1):
+                        Console.Write("Enter the element to push: ");
+                        stackTest.Push(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case (2):
+                        stackTest.Pop();
+                        Console.WriteLine("Pop() successfully completed!");
+                        break;
+
+                    case (3):
+                        Console.Write("Top of stack is: " + stackTest.Peek());
+                        break;
+
+                    case (4):
+                        isStop = true;
+                        break;
+                }
+            }
+            
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Learning/Stack/Stack/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        private int[] array;
+        private bool isEmpty;
+        private int lastElementIndex;
+
+        public Stack(int stackSize)
+        {
+            array = new int[stackSize];
+            isEmpty = true;
+            lastElementIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = 0;
+        }
+
+        public void Pop()
+        {
+            if(!isEmpty)
+            {
+                return;
+            } else
+            {
+                if(lastElementIndex == 0)
+                {
+                    isEmpty = true;
+                } else
+                {
+                    lastElementIndex--;
+                }
+            }
+        }
+
+        public void Push(int value)
+        {
+            if(lastElementIndex == array.Length - 1)
+            {
+                return;
+            } else
+            {
+                lastElementIndex++;
+                array[lastElementIndex] = value;
+            }
+        }
+
+        public int Peek()
+        {
+            return array[lastElementIndex];
+        }
+    }
+}
