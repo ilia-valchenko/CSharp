b4717b6aff7228338e40a9459e37cfbf7e4e9c34
ivalchenko <i.valchenk@hotmail.com>
2015-09-24 20:25:00 +0300

    Added generic stack

---
+++ Learning/Stack2.0/Stack/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is my custom stack realization based on integer values.\nPlease enter the size of stack: ");
+
+            bool isStop = false;
+            Stack<int> stackTest = new Stack<int>();
+
+            while (!isStop)
+            {
+                Console.WriteLine("\n\nThanks! Now you have to choose one of the proposed operations:\nPush: 1\nPop: 2\nPeek: 3\nPrint stack: 4\nStop: 5\n");
+                int operation = Convert.ToInt32(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case (1):
+                        Console.Write("\nEnter the element to push: ");
+                        stackTest.Push(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case (2):
+                        if (stackTest.Pop() == -1)
+                            Console.WriteLine("\nStack is empty!");
+                        else
+                            Console.WriteLine("\nPop function successfully completed!");
+                        break;
+
+                    case (3):
+                        Console.Write("\nTop of stack is: " + stackTest.Peek());
+                        break;
+
+                    case (4):
+                        stackTest.PrintStack();
+                        break;
+
+                    case (5):
+                        isStop = true;
+                        break;
+                }
+            }
+            
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Learning/Stack2.0/Stack/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<T>
+    {
+        private T[] array;
+        private bool isEmpty { get; set; }
+        private int indexOfLastElement;
+
+        public Stack()
+        {
+            array = new T[10];
+            isEmpty = true;
+            indexOfLastElement = -1;
+        }
+
+        public int Pop()
+        {
+            if(isEmpty)
+                return -1; 
+            else
+            {
+                if(indexOfLastElement == 0)
+                    isEmpty = true;
+
+                indexOfLastElement--;
+                return 1;
+            }
+        }
+
+        public void Push(T value)
+        {
+            isEmpty = false;
+
+            if(indexOfLastElement == array.Length - 1)
+            {
+                // create buffer array
+                T[] bufferArr = new T[array.Length];
+
+                for(int i = 0; i < array.Length - 1; i++)
+                    bufferArr[i] = array[i];
+
+                // increase the size of the old array
+                array = new T[2 * array.Length];
+
+                for (int i = 0; i < bufferArr.Length - 1; i++)
+                    array[i] = bufferArr[i];
+            }
+
+            indexOfLastElement++;
+            array[indexOfLastElement] = value;
+        }
+
+        public T Peek()
+        {
+            return array[indexOfLastElement];
+        }
+
+        public void PrintStack()
+        {
+            Console.WriteLine("\nAll stack's elements:");
+
+            for (int i = indexOfLastElement; i != -1; i--)
+                Console.WriteLine(array[i].ToString());
+        }
+    }
+}
