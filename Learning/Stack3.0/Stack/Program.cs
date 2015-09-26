9262711e1f456bb57989088cc83a65c2bb8c0f1b
ivalchenko <i.valchenk@hotmail.com>
2015-09-26 13:57:23 +0300

    Fix memory leak bug. Correct isEmpty property.

---
+++ Learning/Stack3.0/Stack/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("This is my custom stack realization.");
+            Console.ResetColor();
+
+            bool isStop = false;
+            Stack<int> stackTest = new Stack<int>();
+
+            while (!isStop)
+            {
+                Console.WriteLine("\n\nThanks! Now you have to choose one of the proposed operations:\nPush: 1\nPop: 2\nPeek: 3\nPrint stack: 4\nStop: 5\n");
+                int operation = Convert.ToInt32(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case (1):
+                        Console.Write("\nEnter the element to push: ");
+                        stackTest.Push(Convert.ToInt32(Console.ReadLine()));
+                        break;
+
+                    case (2):
+                        stackTest.Pop();
+                        break;
+
+                    case (3):
+                        Console.Write("\nTop of stack is: " + stackTest.Peek());
+                        break;
+
+                    case (4):
+                        Console.WriteLine("\nStack's elements:");
+
+                        foreach(int item in stackTest.GetArrayOfStackElements())
+                            Console.WriteLine(item);
+
+                        break;
+
+                    case (5):
+                        isStop = true;
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Learning/Stack3.0/Stack/Stack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<T>
+    {
+        private T[] array;
+        private int indexOfLastElement;
+        public bool isEmpty { get { return indexOfLastElement < 0; } }
+        
+
+        public Stack()
+        {
+            array = new T[10];
+            indexOfLastElement = -1;
+        }
+
+        public void Pop()
+        {
+            if(!isEmpty)
+            {
+                array[indexOfLastElement] = default(T);
+                indexOfLastElement--;
+            }
+        }
+
+        public void Push(T value)
+        {
+            if(indexOfLastElement == array.Length - 1)
+            {
+                // create buffer array
+                T[] bufferArr = new T[array.Length];
+
+                for(int i = 0; i < array.Length - 1; i++)
+                    bufferArr[i] = array[i];
+
+                // increase the size of the old array
+                array = new T[2 * array.Length];
+
+                for (int i = 0; i < bufferArr.Length - 1; i++)
+                    array[i] = bufferArr[i];
+            }
+
+            indexOfLastElement++;
+            array[indexOfLastElement] = value;
+        }
+
+        public T Peek()
+        {  
+            try
+            {
+                return array[indexOfLastElement];
+            } 
+            catch(Exception ex)
+            {
+                throw new ApplicationException("Error! Stack is empty!", ex);
+            }
+        }
+
+        public T[] GetArrayOfStackElements()
+        {
+            T[] arrayOfStackElements = new T[indexOfLastElement + 1];
+
+            for (int i = 0; i <= indexOfLastElement ; i++)
+                arrayOfStackElements[i] = array[i];
+
+            return arrayOfStackElements;
+        }
+    }
+}
