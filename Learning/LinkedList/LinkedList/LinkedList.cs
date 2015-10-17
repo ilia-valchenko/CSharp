a13977929eab3b797b906fcde44997999d72c8dd
ivalchenko <i.valchenk@hotmail.com>
2015-10-17 21:37:07 +0300

    Added custom generic doublelinked list

---
+++ Learning/LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList<T>
+    {
+        private Node<T>[] elems;
+        //private Navigator nav;
+        private int nearestFreeSpace;
+        private int indexOfTail {get; set;}
+        private int count { get; set; }
+
+        public LinkedList() {
+            elems = new Node<T>[10];
+            indexOfTail = 0;
+            count = 0;
+
+            for (int i = 0; i < elems.Length; i++)
+                elems[i] = null;
+
+            nearestFreeSpace = 0;
+        }
+
+        public void AddToHead(T value)
+        {
+            if (elems[0] == null)
+            {
+                elems[0] = new Node<T>(value, 0, 0);
+                nearestFreeSpace = 1;
+            } else
+            {
+                if(nearestFreeSpace != 0)
+                {
+                    elems[nearestFreeSpace] = new Node<T>(elems[0].value, elems[0].next, 0); 
+                    elems[0] = new Node<T>(value, nearestFreeSpace, indexOfTail);
+                    elems[nearestFreeSpace].prev = 0;
+                    // update nearestFreeSpace
+                    for(int i = 0; i < elems.Length; i++)
+                    {
+                        if (elems[i] == null)
+                            nearestFreeSpace = i;
+                    }
+                }
+            }
+
+        }
+
+        public void AddToTail(T value)
+        {
+            if (elems[0] == null)
+            {
+                elems[0] = new Node<T>(value, 0, 0);
+                nearestFreeSpace = 1;
+            }
+            else
+            {
+                if (nearestFreeSpace != 0)
+                {
+                    elems[nearestFreeSpace] = new Node<T>(value, 0, indexOfTail);
+                    // old tail
+                    elems[indexOfTail].next = nearestFreeSpace;
+                    // new index of tail
+                    indexOfTail = nearestFreeSpace;
+                    
+                    // update nearestFreeSpace
+                    for (int i = 0; i < elems.Length; i++)
+                    {
+                        if (elems[i] == null)
+                            nearestFreeSpace = i;
+                    }
+                }
+            }
+        }
+
+        public Node<T>[] GetList()
+        {
+            return elems;
+        }
+    }
+
+    class Node<T>
+    {
+        public T value { get; set; }
+        // contains index number
+        public int next { get; set; }
+        public int prev { get; set; }
+
+        public Node() { } // default
+
+        public Node(T value, int next, int prev)
+        {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    struct Navigator
+    {
+        public int next { get; set; }
+        public int prev { get; set; }
+    }
+}
---
+++ Learning/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("CUSTOM DOUBLELINKED LIST\n");
+            Console.ResetColor();
+
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddToHead(3);
+            list.AddToHead(4);
+            list.AddToHead(5);
+            list.AddToHead(6);
+
+            list.AddToTail(11);
+            list.AddToTail(12);
+            list.AddToTail(13);
+            list.AddToTail(14);
+
+            foreach(Node<int> item in list.GetList())
+            {
+                if(item != null)
+                {
+                    Console.Write(list.GetList()[item.next].value + "  ");
+                }
+            }
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
