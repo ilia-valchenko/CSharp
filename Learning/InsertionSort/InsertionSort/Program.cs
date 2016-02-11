8317f43ab41036867facf37d5926a84d25695f1a
Ilia Valchenko <i.valchenk@hotmail.com>
2016-02-11 23:20:29 +0300

    Add simple Insertion sort.

---
+++ Learning/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {5,12,1,1,0,56,98,1,23,6,4,7,8,10,100};
+
+            for(int i = 1; i < arr.Length; i++)
+                for(int j = i; j > 0 && arr[j-1] > arr[j]; j--)
+                {
+                    int helper = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = helper;
+                }
+
+            Console.WriteLine("Print result:");
+            foreach(int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+                    
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
