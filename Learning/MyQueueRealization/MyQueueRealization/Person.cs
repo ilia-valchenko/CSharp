c5d41163a25a7c326f7c77f8cb2768aec162c22f
Ilia Valchenko <i.valchenk@hotmail.com>
2016-05-03 09:42:19 +0300

    Add my own realization of Queue.

---
+++ Learning/MyQueueRealization/MyQueueRealization/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueueRealization
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person() { }
+        public Person(string firstname, string lastname, int age)
+        {
+            Age = age;
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Surname: {1}, Age: {2}", FirstName, LastName, Age);
+        }
+    }
+}
---
+++ Learning/MyQueueRealization/MyQueueRealization/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueueRealization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UseGenericQueue();
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+
+        static void GetCoffee(Person p)
+        {
+            Console.WriteLine("{0} got coffee!", p.FirstName);
+        }
+
+        static void UseGenericQueue()
+        {
+            Queue<Person> pQueue = new Queue<Person>();
+
+            pQueue.Enqueue(new Person("Ilia", "Valchenko", 20));
+            pQueue.Enqueue(new Person("Sergey", "Burbon", 22));
+            pQueue.Enqueue(new Person("Anton", "Yakoychyk", 21));
+
+            Console.WriteLine("Who the first in the queue: {0}", pQueue.Peek());
+
+            for(int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    GetCoffee(pQueue.Dequeue());
+                } catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
