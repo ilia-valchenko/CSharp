84f10b230690ccd457ca8c091d88a43c2fbb1fca
Ilia Valchenko <i.valchenk@hotmail.com>
2016-05-03 09:43:19 +0300

    I added my realization of generic stack.

---
+++ Learning/MyStackRealization/MyStackRealization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStackRealization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> myStackInt = new Stack<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                myStackInt.Push(i);
+                Console.Write(myStackInt.Peek() + " ");
+                if (i % 5 == 0)
+                    Console.WriteLine("\nSize of array: " + myStackInt.SizeOfArray + "\n");
+            }
+
+            Console.WriteLine("Peek of stack: " + myStackInt.Peek());
+
+            Console.WriteLine("\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Learning/MyStackRealization/MyStackRealization/Stack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStackRealization
+{
+    class Stack<T>
+    {
+        T[] arr = new T[5];
+        int index = -1;
+        public int SizeOfArray
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+
+        public void Pop()
+        {
+            if(index != -1)
+            {
+                arr[index] = default(T);
+                index--;
+            } else
+            {
+                throw new Exception("Stack is empty!");
+            }
+        }
+
+        public T Peek()
+        {
+            if(index != -1)
+            {
+                return arr[index];
+            } else
+            {
+                throw new Exception("Stack is empty!");
+            }
+        }
+
+        public void Push(T val)
+        {
+            if(index + 1 > arr.Length - 1)
+            {
+                T[] arrBuff = arr.ToArray();
+                arr = new T[2*arrBuff.Length];
+
+                for (int i = 0; i < arrBuff.Length - 1; i++)
+                    arr[i] = arrBuff[i];
+
+                index++;
+                arr[index] = val;
+            } else
+            {
+                index++;
+                try
+                {
+                    arr[index] = val;
+                } catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+        }
+    }
+}
